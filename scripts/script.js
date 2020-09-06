e6baf270467369952b1084b3544943c6eec13e3d
Gabriel Briones <dossantosgabrielbriones@gmail.com>
2020-09-06 18:46:15 -0300

    organized css files, changed javascript functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks = document.getElementsByClassName("nav-links")[0];
+function main() {
+        const toggleButton = document.getElementsByClassName("toggle-button")[0];
+        const navbarLinks = document.getElementsByClassName("nav-links")[0];
 
-toggleButton.addEventListener('click', () => {
-        navbarLinks.classList.toggle('active');
-});
+        toggleButton.addEventListener('click', () => {
+                navbarLinks.classList.toggle('active');
+        }); 
+}
+
+window.onload = main;
