eec8e1d8f8e2139d75a1f7b13e537fbc6870f207
Gabriel Briones <dossantosgabrielbriones@gmail.com>
2020-09-08 23:42:39 -0300

    added info section in the grid

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,19 @@
 function main() {
         const toggleButton = document.getElementsByClassName("toggle-button")[0];
         const navbarLinks = document.getElementsByClassName("nav-links")[0];
-
+        const buttonHome = document.getElementsByClassName("menu-item-home")[0];
+        const buttonUs = document.getElementsByClassName("menu-item-us")[0];
+        
         toggleButton.addEventListener('click', () => {
                 navbarLinks.classList.toggle('active');
-        }); 
+        });
+        buttonUs.addEventListener('click', () => {
+                navbarLinks.classList.toggle('active');
+        });
+
+        buttonHome.addEventListener('click', () => {
+                navbarLinks.classList.toggle('active');
+        });
 }
 
 window.onload = main;
